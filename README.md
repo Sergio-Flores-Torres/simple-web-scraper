0db9d26f68f4b8182cc3041d1732eba7c4fceeef
Sergio Flores <sergio@saft.industries>
2023-11-08 08:36:04 -0600

    basic download + imports

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A simple web scraper with Node &amp; Cheerio, using Docker
 
 ### Notes
-Uses javascript
+Uses javascript.
+App will create a subfolder called "downloads" in the current installation directory, to save web pages and images, each on its own subfolder.
 
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 // Sergio Flores - sergio@saft.industries
 // Reads web addresses from arguments and saves the html and images to file, along with some metadata
+import * as url from "url";
+import fetch from "node-fetch";
 
 async function main() {
 	const version = process.env.npm_package_version;
@@ -11,19 +13,42 @@ async function main() {
 		console.log("Error: Please supply at least 1 website address.");
 	}
 
-  
-	// Establish connection to the cluster
-	//await establishConnection();
-  
+	for (let i = 2; i < process.argv.length; i++) { // first 2 elements are not relevant (node & index.js)
+
+		if (!stringIsAValidUrl(process.argv[i])) {
+			console.log(`Warn: Supplied address ${process.argv[i]} is not a valid URL. Skipping.`);
+			continue;
+		}
+
+		// Download files
+		await downloadFiles(process.argv[i]);
+ 
+	} 
+ 
 	console.log('Success');
 }
-  
-if (require.main === module) {
-	main().then(
-		() => process.exit(),
-		err => {
-		  console.error(err);
-		  process.exit(-1);
-		},
-	);
+
+async function downloadFiles(website) {
+	const response = await fetch(website);
+	const body = await response.text();
+	console.log(body); // prints a chock full of HTML richness
+	return body;
 }
+
+// Basic validation for provided URL's. Note: Very basic, a more detailed validation should be used in production
+function stringIsAValidUrl(s) {
+    try {
+		new url.URL(s);
+		return true;
+    } catch (err) {
+		return false;
+    }
+};
+
+main().then(
+	() => process.exit(),
+	err => {
+		console.error(err);
+		process.exit(-1);
+	},
+);
