721cf97440aa077adae8e39ae4c6d658f385c188
Sergio Flores <sergio@saft.industries>
2023-11-08 10:32:24 -0600

    Metadata extraction

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A simple web scraper with Node &amp; Cheerio, using Docker
 
 ### Notes
 Uses javascript.
-App will create a subfolder called "downloads" in the current installation directory, to save web pages and images, each on its own subfolder.
+App will create a subfolder called "downloads" in the current installation directory, to save web pages and images, each on its own subfolder named as the domain of the website in question.
+The retrieved page is assumed to be HTML, and an index.html will be created for its content.
+Arbitrarily limited to 100 websites, since for safety reasons there should always be limits to everything.
 
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // index.js
 // Sergio Flores - sergio@saft.industries
 // Reads web addresses from arguments and saves the html and images to file, along with some metadata
+
 import URL from "url";
 import fetch from "node-fetch";
 import fs from "fs";
+import { load } from 'cheerio';
 
 async function main() {
 	const version = process.env.npm_package_version;
@@ -41,19 +43,52 @@ async function downloadFiles(website) {
 
 		const url = new URL.URL(website);	
 		const dirName = process.cwd() + "/downloads/" + url.hostname;
-		console.log(`Status: Saving website ${website} to folder ${dirName}`); 
+		console.log(`Status: Saving website ${website} to folder ${dirName}.`); 
 
 		if (!fs.existsSync(dirName)) {
 			fs.mkdirSync(dirName);
 		}
 
 		const fileName = dirName + "/index.html";
-		console.log(fileName)
 		fs.writeFileSync(fileName, body);
-		
 
+		console.log(`OK: Downloaded website ${website}`); 
+
+		await extractMetadata(website, dirName, body);
+		
 	} catch (err) {
-		console.log(`Error: Could not download website ${website}`); 
+		console.log(`Error: Could not download website ${website}.`); 
+		console.log(err); 
+	}
+}
+
+async function extractMetadata(website, dirName, body) {
+	console.log(`Status: Saving metadata for website ${website}...`); 
+
+	try {
+		const dt = new Date(); // Last fetch
+		let metadata = website + "\n";
+		metadata += "Last fetch: " + dt.toLocaleDateString() + "\n";
+
+		// Load the HTML in Cheerio
+		const $ = load(body);
+			
+		// Select all anchor tags from the page
+		const links = $("a");
+		metadata += "Number of Links: " + links.length + "\n";
+
+		// Select all images from the page
+		const images = $("img");
+		metadata += "Number of Images: " + images.length + "\n";
+
+		const fileName = dirName + "/metadata.txt";
+		fs.writeFileSync(fileName, metadata);
+
+		console.log(`OK: Metadata ready for website ${website} and saved to ${fileName}.`); 
+		console.log(metadata); 
+		
+	} catch (err) {
+		console.log(`Error: Could not extract or save metadata for website ${website}.`); 
 		console.log(err); 
 	}
 }
